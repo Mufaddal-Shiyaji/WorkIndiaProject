eabf0a4cbd9f76963902fe67d90555084a489e0c
aiinterviewpro <mufaddal1693@gmail.com>
2024-07-02 10:59:26 +0530

    final

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,7 +1,6 @@
 import db from "../main.js";
 
 const createUsers = (req, res) => {
-  const post = req.body;
   const search = `SELECT user_id FROM Users WHERE email='${req.body.email}'`;
   db.query(search, (err, result) => {
     if (err) {
@@ -46,6 +45,9 @@ const loginUsers = (req, res) => {
         res.status(500).send("Error logging in");
         return;
       }
+      if (result.length <= 0) {
+        res.status(400).send("Invalid credentials");
+      }
       console.log(result);
       res.status(200).send(result);
     });
@@ -54,7 +56,7 @@ const loginUsers = (req, res) => {
 
 const getUsers = (req, res) => {
   const id = req.params.id;
-  const sql = `SELECT * FROM Users WHERE id = ${id}`;
+  const sql = `SELECT * FROM Users WHERE user_id = ${id}`;
   db.query(sql, (err, results) => {
     if (err) {
       console.log(err);
@@ -66,47 +68,3 @@ const getUsers = (req, res) => {
 };
 
 export { getUsers, createUsers, loginUsers };
-
-//mongo
-// const create = async (req, res) => {
-//   const user = req.body;
-//   const u = await User.findOne({ email: user.email });
-//   if (u != null) {
-//     res.status(400).json({ message: "User already exists" });
-//     return;
-//   }
-//   const newUser = new User(user, bcrypt.hash(user.password, 12));
-//   try {
-//     await newUser.save();
-//     const token = jwt.sign({ email: user.email, id: user._id }, "test", {
-//       expiresIn: "1h",
-//     });
-//     console.log(token);
-//     res.status(201).json({ result: newUser, token });
-//   } catch (error) {
-//     res.status(409).json({ message: error.message });
-//   }
-// };
-
-// const login = async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//       res.status(404).json({ message: "User doesn't exist" });
-//       return;
-//     }
-//     const isPasswordCorrect = bcrypt.compare(password, user.password);
-//     if (!isPasswordCorrect) {
-//       res.status(400).json({ message: "Invalid credentials" });
-//       return;
-//     }
-//     const token = jwt.sign({ email: user.email, id: user._id }, "test", {
-//       expiresIn: "1h",
-//     });
-//     res.status(200).json({ result: user, token });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
